036786525cda27383d27734dfa63c402f90770ea
haricoddle <hariprasad.r@coddletech.com>
2023-08-31 11:39:58 +0530

    changes added

--- Assignment-5/js/main.js
+++ Assignment-5/js/main.js
@@ -10,7 +10,6 @@ const pin = document.getElementById('pin');
 // eslint-disable-next-line arrow-parens
 form.addEventListener('submit', e => {
   e.preventDefault();
-
   // eslint-disable-next-line no-use-before-define
   validateInputs();
 });
@@ -40,7 +39,7 @@ const validateInputs = () => {
   const placeValue = place.value.trim();
   const companyValue = company.value.trim();
   const pinValue = pin.value.trim();
-  //a:-Checking for empty feilds
+  //a:- Checking for empty feilds
   if (nameValue === '') {
     setError(names, 'Name is required');
   } else {
@@ -49,9 +48,12 @@ const validateInputs = () => {
 
   if (phoneValue === '') {
     setError(phone, 'Phone is required');
-  } else if (phoneValue.length > 10) {
-    //c:minimum length of phone number
+  } else if (phoneValue.length < 10) {
+    // c: Minimum length of phone number
     setError(phone, 'Enter only 10 digits');
+    //b:- Checking for non-integer inputs
+  } else if (typeof phoneValue !== 'number') {
+    setError(phone, 'Enter only integer');
   } else {
     setSuccess(phone);
   }
@@ -69,11 +71,13 @@ const validateInputs = () => {
   }
   if (pinValue === '') {
     setError(pin, 'Enter your Pincode');
+  } else if (typeof phoneValue !== 'number') {
+    setError(pin, 'Enter only integer');
   } else {
     setSuccess(pin);
   }
 };
-//d:-Stroring the entered details to localStrorage.
+// d:- Stroring the entered details to localStrorage.
 // eslint-disable-next-line arrow-parens
 form.addEventListener('submit', e => {
   e.preventDefault();
@@ -87,7 +91,8 @@ form.addEventListener('submit', e => {
   const myobjString = JSON.stringify(myobj);
   localStorage.setItem('myObj', myobjString);
 });
-//e:-Prepopulate the form with value stored in localstorage.
+
+// e:- Prepopulate the form with value stored in localstorage.
 const prePopulateBtn = document.getElementById('prePopulate');
 // eslint-disable-next-line arrow-parens
 prePopulateBtn.addEventListener('submit', e => {
@@ -96,24 +101,20 @@ prePopulateBtn.addEventListener('submit', e => {
   if (localData) {
     // eslint-disable-next-line no-unused-vars
     const myObj = JSON.parse(localData);
-    // document.getElementById('name') = myObj.name;
-    // document.getElementById('phone') = myObj.name;
-    // document.getElementById('place') = myObj.name;
-    // document.getElementById('company') = myObj.name;
-    // document.getElementById('pin') = myObj.name;
     // eslint-disable-next-line no-restricted-globals
-    document.form[form][name].value = myObj.name;
+    document.form[form][names].value = myObj.names;
     document.form[form][phone].value = myObj.phone;
     document.form[form][company].value = myObj.company;
     document.form[form][place].value = myObj.place;
     document.form[form][pin].value = myObj.pin;
   }
 });
+
 //2:- creating a div and button using JS only.
 const secDiv = document.createElement('div');
 secDiv.style.display = 'none';
 document.body.appendChild(secDiv);
-const thirdBtn = document.createElement('btn');
+const thirdBtn = document.createElement('button');
 thirdBtn.textContent = 'Basic details';
 document.body.appendChild(thirdBtn);
 
@@ -124,6 +125,7 @@ thirdBtn.addEventListener('click', e => {
   secDiv.textContent = details;
   secDiv.style.display = 'block';
 });
+
 //3:- Creating a private variable and consoling it by accessing it.
 // eslint-disable-next-line no-unused-vars
 const obj = (function () {
@@ -140,6 +142,7 @@ const obj = (function () {
 console.log(obj.favMovie);
 obj.favMovie = 'Interstellar';
 console.log(obj.favMovie);
+
 //4:- Creating a Static Variable and consoling it by accessing it.
 class statExample {
   static myBike = 'Yamaha';
