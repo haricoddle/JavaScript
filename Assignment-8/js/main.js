94f57671eeef7fcbe5ceb5537c6b0e3ed5a086f5
haricoddle <hariprasad.r@coddletech.com>
2023-09-08 12:17:51 +0530

    Added changes

--- Assignment-8/js/main.js
+++ Assignment-8/js/main.js
@@ -3,7 +3,7 @@
 let htmlContent = '';
 const resultMesg = document.getElementById('tbody');
 const xhr = new XMLHttpRequest();
-xhr.open('get', 'https://jsonplaceholder.typicode.com/users', false);
+xhr.open('get', 'jsonfile/data.json', false);
 xhr.send();
 const result = JSON.parse(xhr.responseText);
 // eslint-disable-next-line no-plusplus
@@ -11,6 +11,6 @@ for (let i = 0; i < result.length; ++i) {
   const arrData = result[i];
   /* eslint-disable-next-line prefer-template, no-unused-expressions,
    no-sequences, no-useless-concat*/
-  htmlContent = htmlContent + '<tr><td>' + arrData.id + '<td>' + '<td>' + arrData.name + '<td>' + arrData.username + '<td></tr><br>';
+  htmlContent += '<tr><td>' + arrData.id + '</td><td>' + arrData.name + '</td><td>' + arrData.username + '</td><td>' + arrData.place + '</td><td>' + arrData.phone + '</td></tr><br>';
 }
 resultMesg.innerHTML = htmlContent;
